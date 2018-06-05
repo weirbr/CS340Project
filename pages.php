b35040abe8ef128ac61e62ef67214e580fffab60
weirbr <weirbr@oregonstate.edu>
2018-06-05 14:19:56 -0700

    for the header

---
+++ pages.php
@@ -0,0 +1,8 @@
+<?php
+//$content holds a 2D array with keys being menu names and values being an array with a subtitle, and content
+	$content = array(
+		"Profile" => "login-signupPage.php",
+		"Home" => "homePage.php",
+		"Search Parks" => "searchParks.php");
+
+?>
