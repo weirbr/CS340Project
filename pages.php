03f574ec11f3c535223ed25dc75643c8517126c9
alexanderniebur <niebura@oregonstate.edu>
2018-06-11 13:34:39 -0700

    Added support for lists, and reordered

--- pages.php
+++ pages.php
@@ -2,7 +2,9 @@
 //$content holds a 2D array with keys being menu names and values being an array with a subtitle, and content
 	$content = array(
 		"Profile" => "login-signupPage.php",
-		"Home" => "homePage.php",
-		"Search Parks" => "searchParks.php");
-
+		"Search Parks" => "searchParks.php",
+		"Park List" => "ParkList.php",
+		"Campsite List" => "CampList.php",
+		"Hike/Trail List" => "HikeList.php",
+		"Home" => "homePage.php");
 ?>
